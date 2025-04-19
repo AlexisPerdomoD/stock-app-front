779a3ed16644d0b72252553b1cde1c90777e9d4f
alexisperdomod <dropdibemol@gmail.com>
2025-04-19 17:02:43 -0500

    feat: create Footer general component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <div class="flex items-center justify-evenly">
+            <p class="text-sm font-medium text-[var(--muted-txt)] hover:text-[var(--primary-txt)]">
+                Copyright © 2025 Stock App
+            </p>
+            <p class="text-sm font-medium text-[var(--muted-txt)]">
+                Make with ❤️ by
+                <a
+                    href="https://github.com/AlexisPerdomoD"
+                    target="_blank"
+                    class="text-[var(--secondary-txt)] hover:text-[var(--primary-txt)]"
+                    >Alexis P</a
+                >
+            </p>
+        </div>
+    </footer>
+</template>
