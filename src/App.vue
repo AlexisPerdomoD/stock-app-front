bcd9453133cb34f910707c4233a94f70e266b88f
alexisperdomod <dropdibemol@gmail.com>
2025-04-29 21:59:35 -0500

    fix: background color

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Footer from './components/Footer.vue'
 </script>
 
 <template>
-    <section class="min-h-screen bg-[var(--secondary-background)]">
+    <section class="min-h-screen bg-[var(--primary-background)]">
         <section class="min-h-[90vh]">
             <RouterView />
         </section>
